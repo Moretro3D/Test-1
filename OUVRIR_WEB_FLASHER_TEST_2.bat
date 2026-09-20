56255e904c352841fc9d02634418d9ed5e0c05ae
Moretro3D <morgan.duncas@gmail.com>
2026-09-20 17:41:00 +0200

    PokeTama V11 officielle

---
+++ OUVRIR_WEB_FLASHER_TEST_2.bat
@@ -0,0 +1,5 @@
+@echo off
+title PokeTama V11 - Web Flasher Test-2
+echo Ouverture du Web Flasher Test-2 sans cache...
+start "" "https://moretro3d.github.io/Test-2/?v=1.47.0-moretro3d-v11-official"
+exit /b 0
